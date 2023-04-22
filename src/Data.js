9ccfce64d83bd0939870efa0431bbf5b5f1c7ffa
Abhishek <as3826967@gmail.com>
2023-04-22 21:48:55 +0530

    final deployment

--- src/Data.js
+++ src/Data.js
@@ -1,94 +1,30 @@
 const Data = [{
     id: "1",
-    firstName: "TestFirst",
-    lastName: "TestLast",
+    firstName: "TestFirst1",
+    lastName: "TestLast1",
     status: "Active"
 },
 {
     id: "2",
-    firstName: "TestFirst",
-    lastName: "TestLast",
+    firstName: "TestFirst2",
+    lastName: "TestLast2",
     status: "Active"
 },
 {
     id: "3",
-    firstName: "TestFirst",
-    lastName: "TestLast",
+    firstName: "TestFirst3",
+    lastName: "TestLast3",
     status: "Active"
 },
 {
     id: "4",
-    firstName: "TestFirst",
-    lastName: "TestLast",
+    firstName: "TestFirst4",
+    lastName: "TestLast4",
     status: "Active"
 }, {
     id: "5",
-    firstName: "TestFirst",
-    lastName: "TestLast",
-    status: "Active"
-},
-{
-    id: "6",
-    firstName: "TestFirst",
-    lastName: "TestLast",
-    status: "Active"
-},
-{
-    id: "7",
-    firstName: "TestFirst",
-    lastName: "TestLast",
-    status: "Active"
-},
-{
-    id: "8",
-    firstName: "TestFirst",
-    lastName: "TestLast",
-    status: "Active"
-},{
-    id: "9",
-    firstName: "TestFirst",
-    lastName: "TestLast",
-    status: "Active"
-},
-{
-    id: "10",
-    firstName: "TestFirst",
-    lastName: "TestLast",
-    status: "Active"
-},
-{
-    id: "11",
-    firstName: "TestFirst",
-    lastName: "TestLast",
-    status: "Active"
-},
-{
-    id: "12",
-    firstName: "TestFirst",
-    lastName: "TestLast",
-    status: "Active"
-}, {
-    id: "13",
-    firstName: "TestFirst",
-    lastName: "TestLast",
-    status: "Active"
-},
-{
-    id: "14",
-    firstName: "TestFirst",
-    lastName: "TestLast",
-    status: "Active"
-},
-{
-    id: "15",
-    firstName: "TestFirst",
-    lastName: "TestLast",
-    status: "Active"
-},
-{
-    id: "16",
-    firstName: "TestFirst",
-    lastName: "TestLast",
+    firstName: "TestFirst5",
+    lastName: "TestLast5",
     status: "Active"
 }]
 
